ead21156b66c5a542dc5edf8660ae47b0b5f925a
Björn Petersen <pheasn@gmail.com>
2016-11-23 18:40:14 +0100

    Document CompositeObservableEventList

--- src/main/java/com/github/bachelorpraktikum/dbvisualization/view/CompositeObservableEventList.java
+++ src/main/java/com/github/bachelorpraktikum/dbvisualization/view/CompositeObservableEventList.java
@@ -4,18 +4,35 @@ import com.github.bachelorpraktikum.dbvisualization.model.Event;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
+
+import javax.annotation.ParametersAreNonnullByDefault;
 
 import javafx.collections.ObservableList;
 import javafx.collections.ObservableListBase;
 
+/**
+ * An {@link ObservableList} implementation that is composed of multiple observable lists each
+ * containing an {@link Event} subtype.
+ */
+@ParametersAreNonnullByDefault
 class CompositeObservableEventList extends ObservableListBase<Event> {
     private final List<ObservableList<? extends Event>> lists;
 
+    /**
+     * Creates a new instance.
+     *
+     * @param eventLists the lists this list is composed of
+     * @throws NullPointerException if eventLists is null
+     */
     CompositeObservableEventList(List<ObservableList<? extends Event>> eventLists) {
-        this.lists = new ArrayList<>(eventLists);
+        this.lists = new ArrayList<>(Objects.requireNonNull(eventLists));
         registerListeners();
     }
 
+    /**
+     * Registers change listeners for all lists this list is composed of.
+     */
     private void registerListeners() {
         for (ObservableList<? extends Event> list : lists) {
             list.addListener(this::fireChange);
