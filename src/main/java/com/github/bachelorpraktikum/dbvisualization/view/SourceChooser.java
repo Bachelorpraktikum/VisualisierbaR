8f5b519be2fd0f5ae7b0f3143a984a749bb1adcc
chabare <chabare95@gmail.com>
2016-11-23 23:04:32 +0100

    Add JavaDoc for the SourceChooser interface

--- src/main/java/com/github/bachelorpraktikum/dbvisualization/view/SourceChooser.java
+++ src/main/java/com/github/bachelorpraktikum/dbvisualization/view/SourceChooser.java
@@ -6,12 +6,43 @@ import java.net.URL;
 
 import javafx.beans.property.ReadOnlyProperty;
 
+/**
+ * Represents a chooser for a data source.<br>The user can input a URL by which the data source can
+ * be accessed.<br>The chooser does not check whether that is the case.<br>The data which can be
+ * retrieved from the source has to be compatible with the described {@link
+ * com.github.bachelorpraktikum.dbvisualization.model model}<br>The {@link DataSource.Type type} of
+ * the source should be unique.
+ *
+ * @see DataSource
+ * @see DataSource.Type
+ */
 interface SourceChooser {
+    /**
+     * Returns the url for the resource
+     *
+     * @return URL for the resource
+     */
     URL getResourceURL();
 
+    /**
+     * Returns the property for the resource url
+     *
+     * @return Property for the resource url
+     */
     ReadOnlyProperty<URL> resourceURLProperty();
 
+    /**
+     * Returns the id of the root pane
+     *
+     * @return ID of the root pane
+     */
     String getRootPaneId();
 
+    /**
+     * Returns the resource {@link DataSource.Type type}
+     *
+     * @return Type of the resource
+     * @see DataSource.Type
+     */
     DataSource.Type getResourceType();
 }
