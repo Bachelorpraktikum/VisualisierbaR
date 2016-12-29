b217fe729468b111aadb0991467e2c6b87187533
chabare <chabare95@gmail.com>
2016-12-29 19:58:33 +0100

    Add controller for database tab

--- src/main/java/com/github/bachelorpraktikum/dbvisualization/view/DatabaseChooserController.java
+++ src/main/java/com/github/bachelorpraktikum/dbvisualization/view/DatabaseChooserController.java
@@ -1,4 +1,9 @@
 package com.github.bachelorpraktikum.dbvisualization.view;
 
+import javafx.scene.control.TextField;
+import javafx.scene.layout.BorderPane;
+
 public class DatabaseChooserController {
+    public BorderPane rootPane;
+    public TextField urlField;
 }
