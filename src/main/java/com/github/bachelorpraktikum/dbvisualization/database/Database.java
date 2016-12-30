1035d6fe730c70ba51b1fb12d416037d674a2317
chabare <chabare95@gmail.com>
2016-12-30 13:25:18 +0100

    Add database initialization

--- src/main/java/com/github/bachelorpraktikum/dbvisualization/database/Database.java
+++ src/main/java/com/github/bachelorpraktikum/dbvisualization/database/Database.java
@@ -1,8 +1,42 @@
 package com.github.bachelorpraktikum.dbvisualization.database;
 
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
+
+import javax.xml.crypto.Data;
+import java.net.URL;
+import java.sql.SQLException;
+
 public class Database implements AutoCloseable {
+    private final URL url;
+    private final DatabaseUser user;
+    private HikariDataSource connection;
+
+    Database(URL url, DatabaseUser user) {
+        this.url = url;
+        this.user = user;
+        HikariConfig config = new HikariConfig();
+        config.setJdbcUrl("jdbc:mysql://" + this.url);
+        config.setUsername(user.getUser());
+        config.setPassword(user.getPassword());
+        config.addDataSourceProperty("cachePrepStmts", "true");
+        config.addDataSourceProperty("prepStmtCacheSize", "250");
+        config.addDataSourceProperty("prepStmtCacheSqlLimit", "2048");
+        connection = new HikariDataSource(config);
+    }
+
+    boolean testConnection() {
+        try {
+            connection.getConnection().getClientInfo();
+        } catch (SQLException ignored) {
+            return false;
+        }
+
+        return true;
+    }
+    
     @Override
     public void close() throws Exception {
-
+        connection.close();
     }
 }
