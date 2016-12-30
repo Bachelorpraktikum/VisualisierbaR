3dd4f5995b87c7900a12a0f45df30c106f20492e
chabare <chabare95@gmail.com>
2016-12-30 13:10:55 +0100

    Add auto closeable Database class

---
+++ src/main/java/com/github/bachelorpraktikum/dbvisualization/database/Database.java
@@ -0,0 +1,8 @@
+package com.github.bachelorpraktikum.dbvisualization.database;
+
+public class Database implements AutoCloseable {
+    @Override
+    public void close() throws Exception {
+
+    }
+}
