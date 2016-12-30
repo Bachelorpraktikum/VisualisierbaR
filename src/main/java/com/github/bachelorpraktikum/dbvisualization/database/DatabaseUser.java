0d8f16ab65b57434d2ba1fd8965649abd1531615
chabare <chabare95@gmail.com>
2016-12-30 13:25:09 +0100

    Implement database user

--- src/main/java/com/github/bachelorpraktikum/dbvisualization/database/DatabaseUser.java
+++ src/main/java/com/github/bachelorpraktikum/dbvisualization/database/DatabaseUser.java
@@ -1,4 +1,19 @@
 package com.github.bachelorpraktikum.dbvisualization.database;
 
 public class DatabaseUser {
+    private final String user;
+    private final String password;
+
+    DatabaseUser(String user, String password) {
+        this.user = user;
+        this.password = password;
+    }
+
+    String getUser() {
+        return user;
+    }
+
+    String getPassword() {
+        return password;
+    }
 }
