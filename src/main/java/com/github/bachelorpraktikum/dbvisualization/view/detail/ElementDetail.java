06a5785597fd7f4774a15d62ec7d288925b3b801
Chabare <chabare95@gmail.com>
2017-01-12 15:24:07 +0100

    Add detail implementations

--- src/main/java/com/github/bachelorpraktikum/dbvisualization/view/detail/ElementDetail.java
+++ src/main/java/com/github/bachelorpraktikum/dbvisualization/view/detail/ElementDetail.java
@@ -1,4 +1,42 @@
 package com.github.bachelorpraktikum.dbvisualization.view.detail;
 
-public class ElementDetail {
+import com.github.bachelorpraktikum.dbvisualization.model.Coordinates;
+import com.github.bachelorpraktikum.dbvisualization.model.Element;
+
+import java.net.URL;
+import java.util.List;
+
+import javax.annotation.Nullable;
+
+public class ElementDetail extends ElementDetailBase {
+    private Element element;
+
+    ElementDetail(Element element) {
+        this.element = element;
+    }
+
+    @Override
+    String getName() {
+        try {
+            return element.getName().split("_")[0];
+        } catch (IndexOutOfBoundsException ignored) {
+            return element.getName();
+        }
+    }
+
+    @Override
+    @Nullable
+    URL getImageURL() {
+        List<URL> imageUrls = element.getType().getImageUrls();
+        if (imageUrls.size() > 0) {
+            return null;
+        }
+
+        return imageUrls.get(0);
+    }
+
+    @Override
+    Coordinates getCoordinates() {
+        return element.getNode().getCoordinates();
+    }
 }
--- src/main/java/com/github/bachelorpraktikum/dbvisualization/view/detail/TrainDetail.java
+++ src/main/java/com/github/bachelorpraktikum/dbvisualization/view/detail/TrainDetail.java
@@ -1,4 +1,42 @@
 package com.github.bachelorpraktikum.dbvisualization.view.detail;
 
-public class TrainDetail {
+import com.github.bachelorpraktikum.dbvisualization.model.Coordinates;
+import com.github.bachelorpraktikum.dbvisualization.model.train.Train;
+
+import java.net.URL;
+
+public class TrainDetail extends ElementDetailBase {
+    private Train train;
+
+    public TrainDetail(Train train) {
+        this.train = train;
+    }
+
+    @Override
+    String getName() {
+        return train.getReadableName();
+    }
+
+    @Override
+    URL getImageURL() {
+        return Train.class.getResource(String.format("../symbols/%s.png", "train"));
+    }
+
+    @Override
+    Coordinates getCoordinates() {
+        // TODO: Decide on which position to display
+        return getState().getPosition().getFrontEdge().getNode1().getCoordinates();
+    }
+
+    Train.State getState() {
+        return train.getState(getTime());
+    }
+
+    int getSpeed() {
+        return getState().getSpeed();
+    }
+
+    int getLength() {
+        return train.getLength();
+    }
 }
