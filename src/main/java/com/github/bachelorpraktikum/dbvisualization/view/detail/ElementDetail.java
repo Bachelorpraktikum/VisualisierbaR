c2e6ba08d22a65115a825cd065478ad9ea600b9e
Chabare <chabare95@gmail.com>
2017-01-12 15:23:53 +0100

    Add element detail classes

---
+++ src/main/java/com/github/bachelorpraktikum/dbvisualization/view/detail/ElementDetail.java
@@ -0,0 +1,4 @@
+package com.github.bachelorpraktikum.dbvisualization.view.detail;
+
+public class ElementDetail {
+}
---
+++ src/main/java/com/github/bachelorpraktikum/dbvisualization/view/detail/ElementDetailBase.java
@@ -0,0 +1,23 @@
+package com.github.bachelorpraktikum.dbvisualization.view.detail;
+
+import com.github.bachelorpraktikum.dbvisualization.model.Coordinates;
+
+import java.net.URL;
+
+abstract class ElementDetailBase {
+    private int time;
+
+    abstract String getName();
+
+    abstract URL getImageURL();
+
+    abstract Coordinates getCoordinates();
+
+    void setTime(int time) {
+        this.time = time;
+    }
+
+    int getTime() {
+        return time;
+    }
+}
