170d5b7331c408ca32e0bb3bec14d3c7ae9258ee
Björn Petersen <pheasn@gmail.com>
2017-03-01 18:14:24 +0100

    Store config keys in enum instead of properties file

---
+++ src/main/java/com/github/bachelorpraktikum/dbvisualization/config/ConfigKey.java
@@ -0,0 +1,22 @@
+package com.github.bachelorpraktikum.dbvisualization.config;
+
+public enum ConfigKey {
+
+    initialDirectory("%s_initialDirectory"),
+    colors("trainColors");
+
+    private final String key;
+
+    ConfigKey(String key) {
+        this.key = key;
+    }
+
+    public String getKey() {
+        return key;
+    }
+
+    @Override
+    public String toString() {
+        return getKey();
+    }
+}
